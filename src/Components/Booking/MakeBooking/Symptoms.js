ef26ca5d3e56d0dc4aa941bc5dd420f8a09469c5
rakibulanas3935 <anas15-3935@diu.edu.bd>
2021-11-14 13:06:20 +0600

    adding checkbox

--- src/Components/Booking/MakeBooking/Symptoms.js
+++ src/Components/Booking/MakeBooking/Symptoms.js
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import DiscreteSliderMarks from "./Text";
 import "./Symptoms.css";
 import { Link } from "react-router-dom";
 const Symptoms = () => {
+  const [checkValue, getcheckValue] = useState([]);
+
+  const handleCheckList = (e) => {
+    getcheckValue(e.target.value);
+  };
+
+  console.log(checkValue);
   return (
     <div className="container-symptoms">
       <div className="container-title">
@@ -28,10 +35,130 @@ const Symptoms = () => {
         <div className="symptom-title">Loss of Function</div>
         <DiscreteSliderMarks></DiscreteSliderMarks>
       </div>
+
       <div className="symptoms">
         <div className="symptom-title">Loss of Motion</div>
         <DiscreteSliderMarks></DiscreteSliderMarks>
       </div>
+
+      <div className="checklist-symptoms">
+        <div className="title-checkliist">Reason for Visit</div>
+        <div className="check-item">
+          <div className="container-checkbox-list">
+            <input
+              type="checkbox"
+              name="symptoms"
+              onChange={handleCheckList}
+              value="New Injury"
+              id=""
+            />
+            <span className="reason-list">New Injury</span>
+          </div>
+          <div className="container-checkbox-list">
+            <input
+              type="checkbox"
+              name="symptoms"
+              onChange={handleCheckList}
+              value="Alleviate Pain"
+              id=""
+            />
+            <span className="reason-list">Alleviate Pain</span>
+          </div>
+          <div className="container-checkbox-list">
+            <input
+              type="checkbox"
+              name="symptoms"
+              onChange={handleCheckList}
+              value="Hydro Therapy"
+              id=""
+            />
+            <span className="reason-list">Hydro Therapy</span>
+          </div>
+          <div className="container-checkbox-list">
+            <input
+              type="checkbox"
+              name="symptoms"
+              onChange={handleCheckList}
+              value="Injury Prevention"
+              id=""
+            />
+            <span className="reason-list">Injury Prevention</span>
+          </div>
+          <div className="container-checkbox-list">
+            <input
+              type="checkbox"
+              name="symptoms"
+              onChange={handleCheckList}
+              value="Massage Therapy"
+              id=""
+            />
+            <span className="reason-list">Massage Therapy</span>
+          </div>
+          <div className="container-checkbox-list">
+            <input
+              type="checkbox"
+              name="symptoms"
+              onChange={handleCheckList}
+              value="Vertigo Treatment"
+              id=""
+            />
+            <span className="reason-list">Vertigo Treatment</span>
+          </div>
+          <div className="container-checkbox-list">
+            <input
+              type="checkbox"
+              name="symptoms"
+              onChange={handleCheckList}
+              value="Stretching and Flexibility"
+              id=""
+            />
+            <span className="reason-list">Stretching and Flexibility</span>
+          </div>
+          <div className="container-checkbox-list">
+            <input
+              type="checkbox"
+              name="symptoms"
+              onChange={handleCheckList}
+              value="Post Operation Therpay"
+              id=""
+            />
+            <span className="reason-list">Post Operation Therpay</span>
+          </div>
+          <div className="container-checkbox-list">
+            <input
+              type="checkbox"
+              name="symptoms"
+              onChange={handleCheckList}
+              value="Management of Condition"
+              id=""
+            />
+            <span className="reason-list">Management of Condition</span>
+          </div>
+          <div className="container-checkbox-list">
+            <input
+              type="checkbox"
+              name="symptoms"
+              onChange={handleCheckList}
+              value="Post-Partrum Exercise Conditoning"
+              id=""
+            />
+            <span className="reason-list">
+              Post-Partrum Exercise Conditoning
+            </span>
+          </div>
+          <div className="container-checkbox-list">
+            <input
+              type="checkbox"
+              name="symptoms"
+              onChange={handleCheckList}
+              value="Other"
+              id=""
+            />
+            <span className="reason-list">Other</span>
+          </div>
+        </div>
+      </div>
+
       <div className="login-btn">
         <Link to="/step5">
           <button type="submit" className="submit-btn">
--- src/Components/Booking/MakeBooking/Time.js
+++ src/Components/Booking/MakeBooking/Time.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Time.css";
+import img from "../../Images/tick.svg";
 const Time = () => {
-  const [time, setTime] = useState();
+  const [time, setTime] = useState("Pick Time");
   const handleTime = (e) => {
     setTime(e.target.value);
   };
@@ -12,43 +13,47 @@ const Time = () => {
       <div className="container-time-heading">
         <h1 className="time-heading-text">PICK A TIME</h1>
       </div>
+
       <div className="container-grid">
         <div className="container-text">Morning</div>
-        <div className="container-text">Morning</div>
+        <div className="container-text">Afternoon</div>
         <div className="btn-time">
-          <button type="submit" onClick={handleTime}>
-            9.00 AM
-          </button>
+          <input type="submit" value="7:00 AM" onClick={handleTime} />
         </div>
         <div className="btn-time">
-          <button type="submit">11.00 AM</button>
+          <input type="submit" value="8:00 AM" onClick={handleTime} />
         </div>
         <div className="btn-time">
-          <button type="submit">12.00 AM</button>
+          <input type="submit" value="9:00 AM" onClick={handleTime} />
         </div>
         <div className="btn-time">
-          <button type="submit">7.00 AM</button>
+          <input type="submit" value="10:00 AM" onClick={handleTime} />
         </div>
         <div className="btn-time">
-          <button type="submit">7.00 AM</button>
+          <input type="submit" value="11:00 AM" onClick={handleTime} />
         </div>
         <div className="btn-time">
-          <button type="submit">7.00 AM</button>
+          <input type="submit" value="12:00 PM" onClick={handleTime} />
         </div>
         <div className="btn-time">
-          <button type="submit">7.00 AM</button>
+          <input type="submit" value="1:00 PM" onClick={handleTime} />
         </div>
         <div className="btn-time">
-          <button type="submit">7.00 AM</button>
+          <input type="submit" value="2:00 PM" onClick={handleTime} />
         </div>
         <div className="btn-time">
-          <button type="submit">7.00 AM</button>
+          <input type="submit" value="3:00 PM" onClick={handleTime} />
         </div>
         <div className="btn-time">
-          <button type="submit">7.00 AM</button>
+          <input type="submit" value="4:00 PM" onClick={handleTime} />
         </div>
       </div>
-      <h2 className="picktime">{time}</h2>
+
+      <div className="show-value">
+        <img src={img} alt="" height="50px" width="50px" />
+        <h2 className="picktime">{time}</h2>
+      </div>
+
       <div className="login-btn">
         <Link to="/symptoms">
           <button type="submit" className="submit-btn">
